fd11fbdf0f26966ca07180a8d916dda2e9ece125
RomanUstychenko <roman.ustychenko@gmail.com>
2022-10-13 18:13:23 +0300

    finish

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,22 +1,24 @@
 import { nanoid } from "nanoid";
-// import { Component } from "react";
 import { useState, useEffect } from "react";
 import  ContactForm  from "./ContactForm/ContactForm"
 import {ContactList} from "./ContactList/ContactList"
 import Filters from "./Filters/Filters"
 import css from "./Contacts.module.css"
 
-
-// import React from 'react'
-
 export default function Contacts() {
 
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(() => {
+    const value = JSON.parse(localStorage.getItem("contacts"));
+    return value ?? [];
+  });
   const [filter, setFilter] = useState("");
 
   useEffect (() => {
-
-  }, []) 
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]) 
+  useEffect(() => {
+    return () => {localStorage.removeItem("contacts")}
+  }, [])
 
   const findID = nanoid();
 
@@ -30,48 +32,26 @@ export default function Contacts() {
         ...data
       }
       return  [...prev, newName]
-    }
-    )
-    // this.setState ((prev) => {
-    //   const newName = {
-    //     id: nanoid(),
-    //     ...data
-    //   }
-    //   return {
-    //     contacts: [...prev.contacts, newName]
-    //   }
-    // }
-    // )
-  }
- const delContacts = (id) => {
+    })
+  };
+
+const delContacts = (id) => {
   setContacts((prev) => {
-    const newGroup = prev.filter((item) => item.id !== id);
-    return {
-       newGroup
-    };
+    return prev.filter((item) => item.id !== id);
   })
-    // this.setState ((prev) => {
-    //   const newGroup = prev.contacts.filter((item) => item.id !== id);
-    //   return {
-    //     contacts: newGroup
-    //   };
-    // })
-  }
-   const duplicateContacts = ({name}) => {
-      // const { contacts } = this.state;
+  };
+
+const duplicateContacts = ({name}) => {
       const result = contacts.find((contact) => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase());
       return result;
-  }
+  };
 
 const  handleChange = (e) => {
     const {value} = e.target;
     setFilter(value)
-  //   this.setState( 
-  //     {[name]: value}
-  //  )
-  }
+  };
+
  const getFilteredContact = () => {
-    // const { contacts, filter } = this.state;
     if (!filter) {
       return contacts;
     }
@@ -82,11 +62,8 @@ const  handleChange = (e) => {
         return result;
       })
       return filteredContact;
-    }
+    };
 
-   
-    // const { filter } = this.state;
-    const filteredContacts = getFilteredContact();
   return (
     <div  className={css.phoneBook}>
       <div className={css.contactForm}>
@@ -102,113 +79,10 @@ const  handleChange = (e) => {
         handleChange={handleChange}
         />
         <ContactList 
-        items={filteredContacts}
+        items={getFilteredContact()}
         delContacts={delContacts} 
         />
       </div>
     </div>
     )
-}
-
-
-// export default class Contacts extends Component  {
-//   state = {
-//     contacts: [],
-//     filter: ''
-//   }
-//   componentDidMount () {
-//     // console.log("didmount")
-//     const contacts = JSON.parse(localStorage.getItem("contacts"));
-//     if (contacts?.length) {
-//       this.setState({contacts})
-//     }
-//   }
-
-//   componentDidUpdate (prevProps, prevState) {
-//     // console.log("compdid")
-//     const {contacts} = this.state;
-//     if (prevState.contacts !== contacts) {
-//       // console.log("compdid")
-//       localStorage.setItem("contacts", JSON.stringify(contacts));
-
-//     }
-//   }
-//   componentWillUnmount() {
-//     console.log("will")
-//     localStorage.removeItem("contacts")
-//   }
-
-//   findID = nanoid();
-//   addContacts = (data) => {
-//     if (this.duplicateContacts(data))
-//     return alert (`${data.name} is already in contact`)
-//     this.setState ((prev) => {
-//       const newName = {
-//         id: nanoid(),
-//         ...data
-//       }
-//       return {
-//         contacts: [...prev.contacts, newName]
-//       }
-//     })
-//   }
-//   delContacts = (id) => {
-//     this.setState ((prev) => {
-//       const newGroup = prev.contacts.filter((item) => item.id !== id);
-//       return {
-//         contacts: newGroup
-//       };
-//     })
-//   }
-//     duplicateContacts = ({name}) => {
-//       const { contacts } = this.state;
-//       const result = contacts.find((contact) => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase());
-//       return result;
-//   }
-
-//   handleChange = (e) => {
-//     const {name, value} = e.target;
-//     this.setState( 
-//       {[name]: value}
-//    )
-//   }
-//   getFilteredContact () {
-//     const { contacts, filter } = this.state;
-//     if (!filter) {
-//       return contacts;
-//     }
-//         const normalizedFilter = filter.toLocaleLowerCase();
-//         const filteredContact = contacts.filter(({name}) => {
-//         const nornalizedName = name.toLocaleLowerCase();
-//         const result = nornalizedName.includes(normalizedFilter);
-//         return result;
-//       })
-//       return filteredContact;
-//     }
-//       render () {
-//         const {findID, addContacts,  delContacts, handleChange} = this;
-//         const { filter } = this.state;
-//         const contacts = this.getFilteredContact();
-//         return (
-//             <div  className={css.phoneBook}>
-//               <div className={css.contactForm}>
-//               <h2 className={css.titlePhoneBook}>PhoneBook</h2>
-//                 <ContactForm 
-//                 onSubmit={addContacts} />
-//               </div>
-//               <div className={css.contacts}>
-//               <h2>Contacts</h2>
-//                 <Filters 
-//                 findID={findID}
-//                 filter={filter}
-//                 handleChange={handleChange}
-//                 />
-//                 <ContactList 
-//                 items={contacts}
-//                 delContacts={delContacts} 
-//                 />
-//               </div>
-//             </div>
-//             )
-// }
-// }
+};
